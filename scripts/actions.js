54d3a2e88e5eeb315df33ea31dbc61a1023c8b76
Alan de Sousa santos <alansantos@Dias.local>
2019-03-13 11:56:46 -0300

    inicio do slide

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,8 +1,9 @@
-const d = document;
-const menu = d.querySelector(".menu");
+window.addEventListener('load', setCard(), false);
+//---> MENU
+var menu = document.querySelector(".menu");
 
-d.querySelector("#idOpenMenu").addEventListener('click', menuControl);
-d.querySelector("#idCloseMenu").addEventListener('click', menuControl); 
+document.querySelector("#idOpenMenu").addEventListener('click', menuControl);
+document.querySelector("#idCloseMenu").addEventListener('click', menuControl); 
 
 function menuControl(){
     if (menu.classList.contains('close')) {
@@ -12,6 +13,33 @@ function menuControl(){
     }
 }
 
-window.onresize = function(){
-    console.log("adksgdihsg");
-};
+//---> SLIDER
+function setCard() {
+    var card = document.getElementsByClassName('card-body');
+    var max = card.length - 1;
+    var actual = 0;
+     
+    var settings = {
+        first: function(){
+            card[actual].classList.add('ativo');
+        },
+        next: function(){
+            card[actual].classList.remove('ativo');
+            actual == max ? actual = 0 : actual++;
+            console.log(actual);
+            card[actual].classList.add('ativo');
+        },
+        prev: function(){
+            card[actual].classList.remove('ativo');
+            actual == 0 ? actual = max : actual--;
+            console.log(actual);
+            card[actual].classList.add('ativo');
+        }
+    }
+    
+    settings.first();
+
+    document.querySelector(".next").addEventListener("click", settings.next, false);
+    document.querySelector(".prev").addEventListener("click", settings.prev, false);
+}
+
