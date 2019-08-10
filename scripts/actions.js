a6ef4b674b02700d66152efcb02c01e5f88b30b0
AlanSantos11 <alansousa@outlook.com>
2019-08-10 13:06:47 -0300

    Redesign #12

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,7 +1,6 @@
 import SmoothScroll from './modules/smooth-scroll.js';
-import MobileMenu from './modules/mobile-menu.js';
-import MainMove from './modules/main-move.js';
-
+import MobileMenu from './modules/mobile-menu.js'
+import MainAnime from './modules/main-anime.js'; 
 
 const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"]');
 smoothScroll.init();
@@ -9,5 +8,5 @@ smoothScroll.init();
 const mobileMenu = new MobileMenu('.menu', '.mobile-button', '.menu-list');
 mobileMenu.init();
 
-const mainMove = new MainMove('#main', '[data-main="image"]');
-mainMove.init();
+const mainAnime = new MainAnime('#main', '[data-main="image"]');
+mainAnime.init();
